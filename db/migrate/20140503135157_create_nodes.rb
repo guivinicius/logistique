7a1402f259de472a3dba02ca564b15da5a1fbb92
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-05-03 10:54:47 -0300

    Annotating models

---
+++ db/migrate/20140503135157_create_nodes.rb
@@ -0,0 +1,10 @@
+class CreateNodes < ActiveRecord::Migration
+  def change
+    create_table :nodes do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :nodes, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503133110) do
+ActiveRecord::Schema.define(version: 20140503135157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,5 +23,13 @@ ActiveRecord::Schema.define(version: 20140503133110) do
   end
 
   add_index "maps", ["name"], name: "index_maps_on_name", unique: true, using: :btree
+
+  create_table "nodes", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "nodes", ["name"], name: "index_nodes_on_name", using: :btree
 
 end
